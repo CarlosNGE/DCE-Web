3a406a5f64a30fdc5c3780e20da2ae9cf232765b
Juan Carlos <jcarlosnge@gmail>
2023-06-19 18:25:56 -0700

    Made navbar responsive

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+
+    // sidebar open close
+    let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+    let closeOpenBtn = document.querySelector(".nav-links .bx-x");
+    let navLinks = document.querySelector(".nav-links");
+
+    menuOpenBtn.addEventListener("click", ()=>{
+        navLinks.style.left="0";
+    });
+
+    closeOpenBtn.addEventListener("click", ()=>{
+        navLinks.style.left="-100%";
+    });
+
+
+    // sidebar sub menu open close
+
+    let servicesArrow = document.querySelector(".services-arrow");
+
+    servicesArrow.addEventListener("click", ()=>{
+        navLinks.classList.toggle("show1");
+    });
+
+}
